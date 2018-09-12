97a64594ece72db7f3ec4fa135e7ff8e886da7b3
David <contact@discutea.com>
2018-09-12 12:17:32 +0200

    Update ApplicationRestController.php

--- Controller/ApplicationRestController.php
+++ Controller/ApplicationRestController.php
@@ -1,5 +1,6 @@
 <?php
 namespace MD\SocomBundle\Controller;
+
 use App\Entity\User;
 use JMS\Serializer\SerializationContext;
 use MD\SocomBundle\Model\OperatorInterface;
@@ -8,16 +9,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 class ApplicationRestController extends Controller
 {
     public function getUsersByOperatorAction($id)
     {
         $operator = $this->getDoctrine()->getManager()->getRepository(OperatorInterface::class)->find($id);
+
         if (null === $operator) {
             throw $this->createNotFoundException('This operator does not exist!');
         }
+
         return $this->createResponse($operator);
     }
+
     public function getOperatorsAction()
     {
         $qb = $this->getDoctrine()->getManager()->createQueryBuilder();
@@ -29,11 +34,14 @@ class ApplicationRestController extends Controller
             ->andWhere('o.demo = 0')
         ;
         $users = $qb->getQuery()->getArrayResult();
+
         foreach($users as $u) {
             $res[$u['oid']][] = $u;
         }
+
         $json = json_encode($res);
         $response = (new Response($json));
+
         return $response;
     }
     /**
@@ -47,16 +55,19 @@ class ApplicationRestController extends Controller
                 array('demo' => false),
                 array('createdAt' => 'asc')
             );
+
         $response = (new JsonResponse(
-            $this->get('jms_serializer')
+            json_decode($this->get('jms_serializer')
                 ->serialize(
                     $operators,
                     'json',
                     SerializationContext::create()->setGroups(array('socom-mini'))
-                )
+                ))
         ));
+
         return $response;
     }
+
     /**
      * @param Request $request
      * @return Response
@@ -71,14 +82,18 @@ class ApplicationRestController extends Controller
             'json'
         );
         $errors = $this->get("validator")->validate($operator);
+
         if (count($errors) > 0) {
             return $this->createResponse($errors, Response::HTTP_BAD_REQUEST);
         }
+
         $em = $this->getDoctrine()->getManager();
         $em->persist($operator);
         $em->flush();
+
         return $this->createResponse($operator);
     }
+
     /**
      * @param $object
      * @param $statusCode
@@ -89,11 +104,14 @@ class ApplicationRestController extends Controller
         if (!is_object($object)) {
             throw new \LogicException('The variable $object is not a object serializable!');
         }
+
         $json = $this->get('jms_serializer')->serialize($object, 'json');
         $response = (new Response($json));
         $response->setStatusCode($statusCode);
+
         return $response;
     }
+
     /**
      * @param $content
      * @return string
@@ -101,8 +119,10 @@ class ApplicationRestController extends Controller
     private function convertToSnakeCase($content)
     {
         $content = $this->arrayToSnakeCase(json_decode($content, true));
+
         return json_encode($content);
     }
+
     /**
      * @param array $array
      * @return array
@@ -112,15 +132,18 @@ class ApplicationRestController extends Controller
         foreach ($array as $key => $value) {
             preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $key, $matches);
             $ret = $matches[0];
+
             foreach ($ret as &$match) {
                 $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
             }
             $key = implode('_', $ret);
             $content[$key] = $value;
+
             if (is_array($value)) {
                 $content[$key] = $this->arrayToSnakeCase($value);
             }
         }
+
         return $content;
     }
 }
